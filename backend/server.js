6d47af994ffe5d575c09d44a756dd7da9b88a3e5
Samarth Pal Antil <antil.samarth@gmail.com>
2023-06-21 14:42:38 +0530

    1.6

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: '*', // allow to server to accept request from different origin
 }));
 
 app.use((req, res, next) => {
